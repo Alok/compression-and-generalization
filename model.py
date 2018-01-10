0e0067f747d25e26ad07017b53bcf5806df7c470
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 23:20:47 -0800

    rm extra layer

--- model.py
+++ model.py
@@ -13,14 +13,12 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(28**2, 512)
         self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 512)
         self.out = nn.Linear(512, 10)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
         x = F.log_softmax(self.out(x))
         return x
 
