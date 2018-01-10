49ff486cdb8f238a8feafb9341d621890c15e43b
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 23:20:20 -0800

    Add compression again

--- main.py
+++ main.py
@@ -25,18 +25,20 @@ plt.ioff()
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-for iter in range(ITERS):
+for iter in range(1, ITERS + 1):
 
     # Train
     real_losses = train(real_model, real=True, epochs=REAL_EPOCHS)
     fake_losses = train(fake_model, real=False, epochs=FAKE_EPOCHS)
 
     # Plot performance
-    plt = plot(real_losses, fake_losses)
     plt = plot(real=real_losses[::STEP_SIZE], fake=fake_losses[::STEP_SIZE])
+    plt.title('Iteration: %s' % iter)
+    plt.ylabel('Loss')
+    plt.xlabel('Minibatch')
     plt.savefig(('figs/%s.png' % iter))
     plt.gcf().clear()
 
     # # Compress and repeat
-    # compress(real_model)
-    # compress(fake_model)
+    compress(real_model)
+    compress(fake_model)
