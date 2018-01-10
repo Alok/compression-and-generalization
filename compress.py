a533b9cd141639349711e8f4e20c6873e0504211
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 16:44:20 -0800

    Add initial copying to model compression

--- compress.py
+++ compress.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import copy
+
 import torch
 
 
 # TODO return copy of model
 def compress(f, THRESHOLD=0.05):
+
+    # TODO return copy of model rather than modifying in-place
+    # f = copy.deepcopy(f)
+
     # mask weights and biases
     for l in f.parameters():
         # 1 if larger, 0 otherwise
         mask = (torch.abs(l.data) > THRESHOLD).float()
         l.data.mul_(mask)
+
+    # return f
 
 
 if __name__ == '__main__':
