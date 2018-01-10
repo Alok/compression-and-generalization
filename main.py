1ee7988a0e29d4f572fc22e6e5d3ee83b81c9ed9
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 16:45:24 -0800

    Rewrite main module
    Link together everything else

--- main.py
+++ main.py
@@ -7,107 +7,31 @@ import matplotlib.pyplot as plt
 import torch
 
 from cli_options import args
+from compress import compress
+from model import fake_model, real_model
+from plot import plot
+from train import train
 
-CYCLES = 5
-
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
+ITERS = 5
 # Set seed
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
+# Don't plot interactively
+plt.ioff()
+
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        'data',
-        train=True,
-        download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307, ), (0.3081, ))
-        ])
-    ),
-    batch_size=args.batch_size,
-    shuffle=True,
-    **kwargs
-)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        'data',
-        train=False,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307, ), (0.3081, ))
-        ])
-    ),
-    batch_size=args.test_batch_size,
-    shuffle=True,
-    **kwargs
-)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+for i in range(ITERS):
 
-x_train = x_train.reshape(-1, 784).astype(np.float32)
-x_test = x_test.reshape(-1, 784).astype(np.float32)
+    # train and plot performance
+    real_losses = train(real_model, real=True)
+    fake_losses = train(fake_model, real=False)
+    plt = plot(real_losses, fake_losses)
+    plt.savefig(f'{i}.png')
+    plt.gcf().clear()
 
-x_train /= 255
-x_test /= 255
-
-y_train = to_categorical(y_train, num_classes=10)
-y_test = to_categorical(y_test, num_classes=10)
-
-x_train = torch.from_numpy(x_train)
-y_train = torch.from_numpy(y_train)
-x_test = torch.from_numpy(x_test)
-x_test = torch.from_numpy(y_test)
-
-
-
-
-def test():
-    # set evaluation mode
-    model.eval()
-
-    test_loss = 0
-    correct = 0
-
-    for data, target in test_loader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-    test_loss /= len(test_loader.dataset)
-    print(
-        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)
-        )
-    )
-
-
-# # loop: training, compression
-# for cycle in range(CYCLES):
-#     for epoch in range(1, args.epochs + 1):
-#         train(epoch)
-#         test()
-#     compress(model)
-
-# TODO mark compression clearly
-# loop: training, compression
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    print(72 * '-')
-    test()
-    compress(model)
-    test()
-
-# TODO drop rows before and after layer you want to prune
-# TODO corrupt input and test it out
-
-if __name__ == '__main__':
-    plt.ioff()
+    # compress and repeat
+    compress(real_model)
+    compress(fake_model)
