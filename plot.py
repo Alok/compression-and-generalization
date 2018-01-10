e0971499bb64e4215558873f5a0bea9f13912104
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 16:20:20 -0800

    rm x-axis manipulation code

--- plot.py
+++ plot.py
@@ -9,23 +9,6 @@ def plot(real, fake):
     xs = np.arange(len(real))
 
     # green for real, red for fake
-    plt.scatter(xs, real, color='g')
-    plt.scatter(xs, fake, color='r')
-    # plt.plot(xs,real,color='g')
-    # plt.plot(xs,fake,color='r')
-
-    # # no x legend
-    # plt.tick_params(
-    #     axis='both',
-    #     which='minor',
-    #     bottom='off',
-    #     top='off',
-    #     labelbottom='off',
-    # )
-
-
-    # TODO no axes marks
-    # TODO no legend
     # plt.scatter(xs, real, color='g')
     # plt.scatter(xs, fake, color='r')
     plt.plot(xs,real,color='g')
