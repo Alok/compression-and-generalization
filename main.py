6fa379dd3bcd0fc853b30a820fab9550c19964d3
Alok Singh <alokbeniwal@gmail.com>
2018-01-09 18:48:08 -0800

    Train real and fake differently

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging
-
+import better_exceptions
 import matplotlib.pyplot as plt
 import torch
 
@@ -11,6 +10,9 @@ from compress import compress
 from model import fake_model, real_model
 from plot import plot
 from train import train
+
+REAL_EPOCHS = 3
+FAKE_EPOCHS = 1 * REAL_EPOCHS
 
 ITERS = 5
 # Set seed
@@ -26,8 +28,10 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 for iter in range(ITERS):
 
     # Train
-    real_losses = train(real_model, real=True)
-    fake_losses = train(fake_model, real=False)
+    # real_losses = train(real_model, real=True)
+    # fake_losses = train(fake_model, real=False)
+    real_losses = train(real_model, real=True, epochs=REAL_EPOCHS)
+    fake_losses = train(fake_model, real=False, epochs=FAKE_EPOCHS)
 
     # Plot performance
     plt = plot(real_losses, fake_losses)
